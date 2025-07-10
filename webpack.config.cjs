3dd04f512fbf791a48ff3f5b5a1e048c8f8e5b8f
Bus42 <greg@studio42dev.com>
2025-07-10 07:11:14 -0600

    Refactor webpack configuration for improved readability and structure

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,46 +1,38 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-
 module.exports = {
-    mode: 'production',
-    entry: './src/main.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
+  mode: 'production',
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    assetModuleFilename: 'assets/[hash][ext][query]', // optional: customize output dir
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif|webm|mp4|svg)$/i,
+        type: 'asset/resource',
       },
-    optimization: {
-        minimize: false
-    },
-    plugins: [
-      // ... other plugins
-  
-      new CopyWebpackPlugin({
-        patterns: [
-          {
-            from: path.resolve(__dirname, 'src/index.html'), // path to your source HTML file
-            to: path.resolve(__dirname, 'dist'), // path to your build destination
-          },
-          {
-            from: path.resolve(__dirname, 'src/config.html'), // path to your source HTML file
-            to: path.resolve(__dirname, 'dist'), // path to your build destination
-          },
-          {
-            from: path.resolve(__dirname, 'src/css'), // path to your source CSS folder
-            to: path.resolve(__dirname, 'dist/css'), // path to your build destination for CSS
-          },
-          {
-            from: path.resolve(__dirname, 'src/lib'), // path to your source CSS folder
-            to: path.resolve(__dirname, 'dist/lib'), // path to your build destination for CSS
-          },
-          // {
-          //   from: path.resolve(__dirname, 'src/img'), // path to your source CSS folder
-          //   to: path.resolve(__dirname, 'dist/img'), // path to your build destination for CSS
-          // },
-          // You can add more patterns for other files or folders as needed
-        ],
-      }),
     ],
-    
-  };
-
+  },
+  optimization: {
+    minimize: false,
+  },
+  performance: {
+    hints: 'warning', 
+    maxAssetSize: 4500000, // 4.5 MB
+    maxEntrypointSize: 4500000, // 4.5 MB
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, 'src/index.html'), to: path.resolve(__dirname, 'dist') },
+        { from: path.resolve(__dirname, 'src/config.html'), to: path.resolve(__dirname, 'dist') },
+        { from: path.resolve(__dirname, 'src/css'), to: path.resolve(__dirname, 'dist/css') },
+        { from: path.resolve(__dirname, 'src/lib'), to: path.resolve(__dirname, 'dist/lib') },
+      ],
+    }),
+  ],
+};
